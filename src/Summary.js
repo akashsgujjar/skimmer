c9d8573f4e057cd2006d4235601969ee1f998d6c
Akash Gujjar <akashgujjar@Akashs-MacBook-Air.local>
2018-12-27 11:48:00 -0800

    push

--- src/Summary.js
+++ src/Summary.js
@@ -10,9 +10,25 @@ class Summary extends React.Component {
     
   truncate() {
       const para = this.props.value;
-      const sentences = para.split(".");
-      const listItems = sentences.map(phrase => <li>{phrase}</li>);
-      return (<ul>{listItems}</ul>);
+//      const sentences = para.split(".");
+//      const listItems = sentences.map(phrase => <li>{phrase}</li>);
+//            return (<ul>{listItems}</ul>);
+
+      const words = para.split(" ");
+      var tally = {};
+      for (var i = 0; i < words.length; i++) {
+          var word = words[i];
+          
+          if(tally[word] == null){
+              tally[word] = 1;
+          }
+          else{
+              tally[word] = tally[word]+1;
+          }
+          //tally[words[i]] = 1;
+      }
+      
+      console.log(tally);
   }
 
   render() {
