5899053290af41de8ba00aacf685571ca3a07493
Akash Gujjar <akashgujjar@Akashs-MacBook-Air.local>
2018-12-30 12:21:51 -0800

    filter done. simplified code.

--- src/Summary.js
+++ src/Summary.js
@@ -6,18 +6,42 @@ class Summary extends React.Component {
   constructor(props) {
     super(props)
     this.truncate = this.truncate.bind(this);
+    this.filtering = this.filtering.bind(this);
+    this.tallying = this.tallying.bind(this);
   }
-    
   truncate() {
-      const para = this.props.value;
-//      const sentences = para.split(".");
-//      const listItems = sentences.map(phrase => <li>{phrase}</li>);
-//            return (<ul>{listItems}</ul>);
-
-      const words = para.split(" ");
-      var tally = {};
-      for (var i = 0; i < words.length; i++) {
-          var word = words[i];
+      var para = this.props.value;
+      console.log('para' + para);
+      const sentences = para.split(".");
+      var listItems;     
+      var filteredSentences = sentences.map(sentence =>this.filtering(sentence));                                          
+      listItems = filteredSentences.map(phrase => <li>{phrase + ' '}</li>);
+      return (<ul>{listItems}</ul>);
+  }
+  filtering(sentence){
+      var words = sentence.split(" ");
+    const remove = [
+        'the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I', 'while', 'still',
+        'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at', 'though', 'i', '—',
+        'this', 'but', 'his', 'by', 'from', 'they', 'we', 'say', 'her', 'she', 'or', 'will', 'an', 
+        'my', 'one', 'all', 'would', 'there', 'their', 'what', 'so', 'up', 'out', 'if', 'about', 
+        'who', 'get', 'which', 'go', 'when', 'me', 'make', 'can', 'like', 'time', 'no', 'just', 
+        'him', 'know', 'take', 'person', 'into', 'year', 'your', 'good', 'some', 'could', 'them', 
+        'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its', 'over', 'think', 
+        'also', 'back', 'after', 'use', 'two', 'how', 'our', 'first', 'well', 'way', 'even', 'has', 
+        'new', 'want', 'because', 'any', 'these', 'give', 'day', 'most', 'us', 'is', 'are', 'was'];
+      
+      const filterWords = words.filter(word1 => remove.indexOf(word1) < 0);
+      var compound = "";
+      for(var i = 0; i < filterWords.length; i++){
+        compound += filterWords[i] + " ";
+      }
+      return compound;
+  }
+  tallying(sentence){
+      var tally = [];
+      for (var i = 0; i < sentence.length; i++) {
+          var word = sentence[i];
           
           if(tally[word] == null){
               tally[word] = 1;
@@ -25,12 +49,8 @@ class Summary extends React.Component {
           else{
               tally[word] = tally[word]+1;
           }
-          //tally[words[i]] = 1;
       }
-      
-      console.log(tally);
   }
-
   render() {
     return (
       <div className="result">
